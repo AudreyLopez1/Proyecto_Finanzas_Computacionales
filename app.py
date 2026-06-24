cb02d6d663e74d06d63dbbe1954abff7b2abf00a
AudreyLopez1 <144165429+AudreyLopez1@users.noreply.github.com>
2026-06-24 14:49:41 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if opcion == "Portafolio Óptimo":
     with st.sidebar:
         st.divider()
         st.markdown("**Parámetros para el portafolio**")
-        num_ports       = st.slider("Número de portafolios", 10000, 3000, 100000, step=10000)
+        num_ports       = st.slider("Número de portafolios", 1000, 3000, 10000, step=1000)
         ventas_en_corto = st.checkbox("Permitir ventas en corto", value=False)
 #----------------------------------------------------------------------
 if opcion == "Perfil de Riesgo":
